0e36d8b4eadb2c798c6d7599fd212ca3e910411b
gtraversa <45489354+gtraversa@users.noreply.github.com>
2021-01-06 17:31:52 +0100

    Add files via upload

---
+++ days_of_the_month_switch/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int daysCalculator(int mm, int yyyy) {
+    int numberOfDays = 0;
+    if (yyyy % 4 == 0 && mm == 2) {
+        numberOfDays = 29;
+    }
+    else if(0 <= yyyy && 1 <= mm && mm <= 12 && yyyy%1 == 0 && mm%1 == 0) {
+        switch (mm) {
+            case 4:
+                numberOfDays = 30;
+            case 6:
+                numberOfDays = 30;
+            case 9:
+                numberOfDays = 30;
+            case 11:
+                numberOfDays = 30;
+                break;
+            case 2:
+                numberOfDays = 28;
+                break;
+            default:
+                numberOfDays = 31;
+        }
+    }
+    else {
+        printf("The input is invalid \n");
+    }
+    return numberOfDays;
+}
+
+int main () {
+    int mm, yyyy;
+    printf ("Enter a month in the form mm:\n");
+    scanf ("%d", &mm);
+    printf ("Enter a year in the form yyyy:\n");
+    scanf ("%d", &yyyy);
+    printf ("You entered: %d",mm);
+    printf("/%d\n",yyyy);
+    int numberOfDays = daysCalculator(mm, yyyy);
+    printf("The month has %d days.", numberOfDays);
+}
